4bcfd8f96b9649d4fdc6739165c2e6ecdeb26c60
godkor200 <godkor200@gmail.com>
2021-12-08 18:38:04 +0900

    기본 스키마 설정중

---
+++ server/exercise/src/test/java/com/example/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class ExerciseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
